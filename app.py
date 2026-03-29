ea25e0035558afdcb0b3d175de9e06a81261083b
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-29 18:13:36 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sqlite3
+import time
+
+# -----------------------------
+# PAGE CONFIG
+# -----------------------------
+st.set_page_config(page_title="PragyanAI AI BI Dashboard", layout="wide")
+st.image("PragyanAI_Transperent.png")
