3a32f2f6471cf115f49fb233bda168f386846cae
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-29 18:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,37 @@ import time
 # -----------------------------
 st.set_page_config(page_title="PragyanAI AI BI Dashboard", layout="wide")
 st.image("PragyanAI_Transperent.png")
+
+# -----------------------------
+# DATABASE SETUP
+# -----------------------------
+conn = sqlite3.connect("users.db", check_same_thread=False)
+c = conn.cursor()
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT,
+    password TEXT
+)
+""")
+conn.commit()
+
+# -----------------------------
+# AUTH FUNCTIONS
+# -----------------------------
+def login_user(username, password):
+    c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+    return c.fetchone()
+
+def register_user(username, password):
+    c.execute("INSERT INTO users VALUES (?,?)", (username, password))
+    conn.commit()
+
+# -----------------------------
+# LOGIN SYSTEM
+# -----------------------------
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+menu = ["Login", "Register"]
+choice = st.sidebar.selectbox("Menu", menu)
